3d15558d7bdf41cbfe778e5ea48fe4e1fdda5980
omar salama <omarsalamaali870@gmail.com>
2026-08-12 14:54:29 +0300

    update auth configuration

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(helmet())
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['http://localhost:5173', process.env.FRONTEND_URL],
     credentials: true,
   })
 )
